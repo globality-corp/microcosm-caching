cde29626aa9d8e95bb57bea2586db9a34f4c9778
Adam Ever-Hadani <adamhadani@gmail.com>
2019-08-27 22:48:34 -0700

    WIP

--- microcosm_caching/decorators.py
+++ microcosm_caching/decorators.py
@@ -2,41 +2,15 @@
 Fluent decorators for resources and handlers.
 
 """
-from microcosm.hooks import on_resolve
-
-from microcosm_pubsub.registry import (
-    PubSubMessageSchemaRegistry,
-    SQSMessageHandlerRegistry,
-    media_type_for,
-)
+from functools import wraps
 
 
-def register_schema(registry, schema_cls):
-    registry.register(media_type_for(schema_cls), schema_cls)
-
-
-def register_handler(registry, schema_cls, handler):
-    registry.register(media_type_for(schema_cls), handler)
-
-
-def handles(schema_cls):
+def cacheable(endpoint):
     """
-    Register a handler, tying it to a specific resource.
-
-    Also registers its schema.
+    Decorate a microcosm controller method for an API route as cacheable.
 
     """
+    @wraps(endpoint)
     def decorator(func):
-        on_resolve(PubSubMessageSchemaRegistry, register_schema, schema_cls)
-        on_resolve(SQSMessageHandlerRegistry, register_handler, schema_cls, func)
         return func
     return decorator
-
-
-def schema(schema_cls):
-    """
-    Register a schema.
-
-    """
-    on_resolve(PubSubMessageSchemaRegistry, register_schema, schema_cls)
-    return schema_cls
